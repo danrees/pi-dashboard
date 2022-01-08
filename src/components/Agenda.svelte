59f93f1252df65e912fd5056818d9df186706414
Daniel Rees <daniel_james_rees@hotmail.com>
2022-01-08 09:02:18 -0500

    Fresh setup of a pi dashboard using svelte, tauri, storybook and tailwindcss

---
+++ src/components/Agenda.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Item from "./agenda/Item.svelte";
+
+  let agendaItems = [
+    {
+      id: 1,
+      value: "Shopping",
+    },
+    {
+      id: 2,
+      value: "Get gas",
+    },
+  ];
+</script>
+
+<div>
+  <ul class="list-disc list-outside">
+    {#each agendaItems as item (item.id)}
+      <li class="bg-blue-500">
+        <Item item={item.value} />
+      </li>
+    {/each}
+  </ul>
+</div>
