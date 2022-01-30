036653a43dce4933166a8d7d8e8b1681f4736b2d
Daniel Rees <daniel_james_rees@hotmail.com>
2022-01-29 21:56:54 -0500

    So the refresh functionality works ...
    ish. There are a lot of unwraps in there that need to be unwound,
    but this gets something that works

--- src-tauri/src/google/client.rs
+++ src-tauri/src/google/client.rs
@@ -5,6 +5,8 @@ use crate::auth::MyToken;
 use crate::errors::DashboardError;
 use oauth2::TokenResponse;
 use reqwest::blocking::{Client as RClient, ClientBuilder};
+use retry::delay::Fixed;
+use retry::retry;
 use serde::{Deserialize, Serialize};
 use std::ops::{Deref, DerefMut};
 use std::sync::{Arc, Mutex};
@@ -23,103 +25,82 @@ pub struct Calendar {
 
 pub struct Client {
   url: String,
+  redirect_url: String,
   agent: Agent,
+  token: Option<MyToken>,
 }
 
 impl Client {
   pub fn new(url: Option<String>, token: Option<MyToken>, redirect_url: String) -> Self {
-    let mut ab = ureq::AgentBuilder::new()
+    let ab = ureq::AgentBuilder::new()
       .timeout_read(Duration::from_secs(5))
       .timeout_write(Duration::from_secs(5));
     //.build();
-    if let Some(tk) = token {
-      let mw = Auth {
-        token: tk,
-        redirect_url,
-      };
-      ab = ab.middleware(AuthMiddleware::new(mw))
-    };
     let agent = ab.build();
     match url {
-      Some(url) => Client { url, agent },
+      Some(url) => Client {
+        url,
+        agent,
+        token,
+        redirect_url,
+      },
       None => Client {
         url: GOOGLE_URL.to_string(),
         agent,
+        token,
+        redirect_url,
       },
     }
   }
 
-  pub fn list_calendars(&self) -> Result<String, DashboardError> {
+  fn set_token(&mut self, token: Option<MyToken>) {
+    self.token = token;
+  }
+
+  fn with_retry(&mut self, method: &str, path: &str) -> Result<Response, ureq::Error> {
+    //let token = self.token.as_ref().unwrap();
+    let response = self
+      .agent
+      .request(method, path)
+      .set(
+        "Authorization",
+        format!(
+          "Bearer {}",
+          self.token.as_ref().unwrap().access_token().secret()
+        )
+        .as_str(),
+      )
+      .call();
+    match response {
+      Err(ureq::Error::Status(401, _)) => {
+        let refresh_token =
+          auth::refresh_token(self.token.as_ref().unwrap(), self.redirect_url.clone());
+        let refresh_token2 = refresh_token.unwrap().clone();
+        self.set_token(Some(refresh_token2.clone()));
+        self
+          .agent
+          .request(method, path)
+          .set(
+            "Authorization",
+            format!("Bearer {}", refresh_token2.clone().access_token().secret()).as_str(),
+          )
+          .call()
+      }
+      Err(e) => Err(e),
+      Ok(r) => Ok(r),
+    }
+  }
+
+  pub fn list_calendars(&mut self) -> Result<String, DashboardError> {
     println!("called list_calendars");
     let url = format!("{}{}", self.url, "/users/me/calendarList");
-    let caller = self.agent.get(url.as_str());
-    let response = match caller.call() {
+    let response = match self.with_retry("get", url.as_str()) {
       Ok(resp) => resp,
       Err(e) => return Err(e.into()),
     };
     match response.into_string() {
       Ok(resp) => Ok(resp),
       Err(e) => Err(e.into()),
-    }
-  }
-}
-
-struct Auth {
-  token: MyToken,
-  redirect_url: String,
-}
-
-struct AuthMiddleware(Arc<Mutex<Auth>>);
-
-impl AuthMiddleware {
-  fn new(auth: Auth) -> Self {
-    AuthMiddleware(Arc::new(Mutex::new(auth)))
-  }
-}
-
-impl ureq::Middleware for AuthMiddleware {
-  fn handle(&self, request: Request, next: MiddlewareNext) -> Result<Response, ureq::Error> {
-    println!("in auth middleware");
-    // FIXME: unwrap
-
-    let mut auth_holder = self.0.lock().unwrap();
-    let response = next.handle(request.set(
-      "Authorization",
-      format!("Bearer {}", auth_holder.token.access_token().secret()).as_str(),
-    ));
-    match response {
-      Ok(resp) => {
-        println!("ok response {:?}", resp);
-        if resp.status() == 401 {
-          println!("handing ok 401");
-          println!("after lock");
-          let refreshed =
-            auth::refresh_token(&auth_holder.token, auth_holder.redirect_url.clone()).unwrap();
-          if let Err(err) = auth::save_token(&refreshed) {
-            println!("resp status {}", err);
-            return Err(ureq::Error::Status(401, resp));
-          };
-          auth::save_token(&refreshed);
-          auth_holder.token = refreshed;
-        }
-        Ok(resp)
-      }
-      Err(ureq::Error::Status(401, resp)) => {
-        println!("returned 401 err: {:?}", resp);
-        // FIXME: unwrap
-        let mut auth_holder = self.0.lock().unwrap();
-        let refreshed =
-          auth::refresh_token(&auth_holder.token, auth_holder.redirect_url.clone()).unwrap();
-        auth::save_token(&refreshed);
-        auth_holder.token = refreshed;
-        //next.handle(request)
-        Err(ureq::Error::Status(401, resp))
-      }
-      Err(ureq::Error::Status(code, resp)) => {
-        println!("{} -> {:?}", code, resp);
-        Err(ureq::Error::Status(code, resp))
-      }
-      Err(e) => Err(e),
     }
   }
 }
