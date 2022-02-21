0f3a375cc743523e9030f8eca78d0cf9821632fa
Daniel Rees <daniel_james_rees@hotmail.com>
2022-02-21 10:16:22 -0500

    Add 5 minute timer for polling the weather

--- src/components/weather/weather.svelte
+++ src/components/weather/weather.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/tauri";
   import Card from "../../layout/card.svelte";
+  import WeatherCard from "./weatherCard.svelte";
   type WeatherResponse = {
     main: {
       temp: number;
       humidity: number;
     };
   };
+
   let weather = invoke<WeatherResponse>("get_weather");
+  let refreshedAt = new Date();
+
+  setInterval(() => {
+    weather = invoke<WeatherResponse>("get_weather");
+    refreshedAt = new Date();
+  }, 1000 * 60 * 5);
 </script>
 
 <Card>
@@ -15,8 +23,11 @@
   {#await weather}
     <p>loading...</p>
   {:then val}
-    <p>Temperature: {val.main.temp.toPrecision(4)}</p>
-    <p>Humidity: {val.main.humidity}</p>
+    <WeatherCard
+      at={refreshedAt}
+      temp={+val.main.temp.toPrecision(2)}
+      humidity={val.main.humidity}
+    />
   {:catch err}
     <p>{err}</p>
   {/await}
---
+++ src/components/weather/weatherCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let temp = 0;
+  export let humidity = 0;
+  export let at = new Date();
+</script>
+
+<div>
+  <p>At: {at}</p>
+  <p>Temperature: {temp}</p>
+  <p>Humidity: {humidity}</p>
+</div>
