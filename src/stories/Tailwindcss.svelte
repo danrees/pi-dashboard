4be5df0f80a6c73cba1f775906bfcf156a153eaa
Daniel Rees <daniel_james_rees@hotmail.com>
2022-02-27 20:32:57 -0500

    Storybook still doesn't work

---
+++ src/stories/Tailwindcss.svelte
@@ -0,0 +1,11 @@
+<script lang="ts"></script>
+
+<div>
+  <slot />
+</div>
+
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
--- src/stories/components/agenda.stories.svelte
+++ src/stories/components/agenda.stories.svelte
@@ -2,7 +2,7 @@
   import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
   import Agenda from "../../components/agenda/agenda.svelte";
   import { mockIPC } from "@tauri-apps/api/mocks";
-  import "../../app.css";
+  import Tailwindcss from "../Tailwindcss.svelte";
 
   mockIPC((cmd, args) => {
     switch (cmd) {
@@ -12,6 +12,13 @@
             {
               id: "1",
               summary: "This is an event",
+              start: {
+                dateTime: new Date(),
+              },
+            },
+            {
+              id: "2",
+              summary: "This is another event",
               start: {
                 dateTime: new Date(),
               },
@@ -28,12 +35,14 @@
 <Meta title="Agenda" component={Agenda} />
 
 <Template let:args>
-  <Agenda {...args} />
+  <Tailwindcss>
+    <Agenda {...args} />
+  </Tailwindcss>
 </Template>
 
 <Story name="Primary" args={{}} />
 
-<style lang="postcss">
+<style lang="postcss" global>
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
